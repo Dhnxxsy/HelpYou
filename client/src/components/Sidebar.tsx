aca2a3375134e34ff888ea7a2e4ae8ec52c615a8
File Organizer <file-organizer@localhost>
2026-09-09 12:02:16 +0700

    v1.0.48: perbaiki preview hasil rekaman tidak muncul (CSP blob) - Content-Security-Policy tidak mengizinkan blob: sehingga <video src=blob...> untuk pratinjau hasil rekaman (MP4/WebM) diblokir (preview terlihat hitam/hilang setelah stop walau video ter-rekam); tambahkan blob: ke media-src, connect-src, dan img-src; live preview (srcObject) tidak terpengaruh; terverifikasi recap MP4 & WebM dapat dimuat, berputar, dan berisi data asli

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ import ThemePicker from './ThemePicker';
 import LanguagePicker from './LanguagePicker';
 import { useI18n } from '../lib/i18n';
 
-const VERSION = '1.0.47';
+const VERSION = '1.0.48';
 
 interface SidebarProps {
   current: string;
